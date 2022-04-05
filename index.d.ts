3f49839db0c8042bb9c30acc4766fdad2276d617
vuduc4793 <54024192+vuduc4793@users.noreply.github.com>
2022-04-05 11:19:49 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,4 @@ import * as ReactNative from 'react-native';
 
 export function convertMovToMp4(filePath: string, fileName: string): Promise<any> ;
 export function removeConvertedVideo(fileName: string): Promise<any> ;
+export function checkDirectionExist(): Promise<any> ;
