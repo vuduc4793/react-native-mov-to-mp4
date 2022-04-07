2a620690cf485b1a02573617eab1c7a0b421bd3f
hieunx2 <hieunx2@fabbi.io>
2022-04-07 14:43:41 +0700

    format code

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,9 @@
-import * as React from 'react';
-import * as ReactNative from 'react-native';
+import * as React from "react";
+import * as ReactNative from "react-native";
 
-export function convertMovToMp4(filePath: string, fileName: string): Promise<any> ;
-export function removeConvertedVideo(fileName: string): Promise<any> ;
-export function checkDirectionExist(): Promise<any> ;
+export function convertMovToMp4(
+  filePath: string,
+  fileName: string
+): Promise<any>;
+export function removeConvertedVideo(fileName: string): Promise<any>;
+export function checkDirectionExist(): Promise<any>;
